f44981690d37265b115a23ed80461ffadd163662
AnnaKul <89269690212@mail.ru>
2023-06-22 17:48:37 +0300

    Seminar3 Home work Algoritms

---
+++ src/main/java/org/example/Seminar3/Main.java
@@ -0,0 +1,18 @@
+package org.example.Seminar3;
+
+public class Main {
+    public static void main(String[] args) {
+        LinList list = new LinList();
+        list.addElements(1);
+        list.addElements(0);
+        list.addElements(2);
+        list.addElements(5);
+        list.addElements(10);
+        list.print();
+        list.revert();
+        System.out.println();
+        list.print();
+
+    }
+
+}
