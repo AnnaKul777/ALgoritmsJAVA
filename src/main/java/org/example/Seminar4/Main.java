edc4c56975b6da0958727683e7a03f9cf145321c
AnnaKul <89269690212@mail.ru>
2023-06-23 16:08:59 +0300

    Seminar4 Home work Algoritms

---
+++ src/main/java/org/example/Seminar4/Main.java
@@ -0,0 +1,15 @@
+package org.example.Seminar4;
+
+public class Main {
+    public static void main(String[] args) {
+        RedBlackTree tree = new RedBlackTree();
+        tree.add(5);
+        tree.add(4);
+        tree.add(6);
+        tree.add(8);
+        tree.add(6);
+        tree.add(2);
+        tree.add(3);
+        tree.add(12);
+    }
+}
